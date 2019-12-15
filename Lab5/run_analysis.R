cd9ee5084638b9ebd03702413c62b02bb61fc8da
MariiaUkrainets <47226385+MariiaUkrainets@users.noreply.github.com>
2019-12-15 19:21:08 +0200

    Update run_analysis.R

--- Lab5/run_analysis.R
+++ Lab5/run_analysis.R
@@ -22,7 +22,7 @@ df <- cbind(rbind(x_train, x_test), rbind(y_train, y_test), rbind(subj_train, su
 
 #2. Витягує лише вимірювання середнього значення та стандартного відхилення (mean and standard deviation) для кожного вимірювання. 
 
-statistics <- select(data2,  matches("mean\\(\\)|std\\(\\)|Subject|Activity"))
+statistics <- select(df,  matches("mean\\(\\)|std\\(\\)|Subject|Activity"))
 
 #3. Використовує описові назви діяльностей (activity) для найменування діяльностей у наборі даних. 
 
